ea33cf9ea93c974a7fd26e776dd636645ff44e69
Viji Ramu <vijiramu@gmail.com>
2025-06-30 22:22:26 -0500

    commit Done

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -17,16 +17,64 @@ Include at least one edge case, like NaN, undefined, or null .
 Use console.log() to clearly show the before-and-after type conversions.
 
 */
-
-
-let result = "5" - 2;
+//Part 1. Debuggung Challenge
+//covert the string "5" to Integer 5 using Number() 
+let num = "5"; 
+let result = Number(num) - 2;
 console.log("The result is: " + result);
 
-let isValid = Boolean("false");
-if (isValid) {
-    console.log("This is valid!");
-}
 
+//convert the string "false" to Boolean() result true.
+let isValid = "false";
+console.log("Before conversion the output is a String " + isValid); 
+if (Boolean(isValid)) {
+    console.log("After type converstion is Boolean value " + Boolean(isValid));
+    console.log("This is Valid");
+}  
+
+// convert the number 5 to String() "5"
 let age = "25";
-let totalAge = age + 5;
+let addMore = 5;
+let totalAge = age + String(addMore);
 console.log("Total Age: " + totalAge);
+
+
+//Part 2 example of implicit type conversion.
+// Arithmetic operation of undefined with number, boolean or null gives NaN
+
+let impResult;
+impResult = 4 + undefined;
+console.log(impResult);  // NaN
+
+impResult = 4 - undefined;
+console.log(impResult);  // NaN
+
+impResult = true + undefined;
+console.log(impResult);  // NaN
+
+impResult = null + undefined;
+console.log(impResult);  // NaN
+
+// Example of explicit type conversion.
+
+
+//Convert to Boolean Explicitly
+//To convert other data types to a boolean, you can use Boolean().
+
+//In JavaScript, undefined, null, 0, NaN, ” converts to false. For example,
+
+
+let expResult = Boolean('');
+console.log(expResult); // false
+
+expResult = Boolean(0);
+console.log(expResult); // false
+
+expResult = Boolean(undefined);
+console.log(expResult); // false
+
+expResult = Boolean(null);
+console.log(expResult); // false
+
+expResult = Boolean(NaN);
+console.log(expResult); // false
